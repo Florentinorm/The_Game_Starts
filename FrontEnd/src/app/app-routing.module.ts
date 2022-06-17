d841958686212d1448bc37d2bd778a75300dfece
Joan salazar <diegosalazarh90@gmail.com>
2022-06-17 14:33:50 -0500

    Se sube la primera versión de las pantallas del cliente.

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TableroProductosComponent } from './pages/cliente/tablero-productos/tablero-productos.component';
+import { CaracteristicaProductosComponent } from './pages/cliente/caracteristica-productos/caracteristica-productos.component';
+import { ProcesoComprasComponent } from './pages/cliente/proceso-compras/proceso-compras.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'home', component: TableroProductosComponent},
+  {path: 'caracteristicas_producto', component: CaracteristicaProductosComponent},
+  {path: 'proceso_compra', component: ProcesoComprasComponent},
+  {path: '', redirectTo: "/home", pathMatch: "full"},
+  {path: '**', redirectTo: "/home"},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
