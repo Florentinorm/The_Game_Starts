2f59e07f678d4db859f4dc118c4dcad3b1e5ff06
an222xd <joseantonio222xd@gmail.com>
2022-07-05 18:02:35 -0500

    Rutas busqueda por id

--- Backend/routes/admin.route.js
+++ Backend/routes/admin.route.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const productoCotroller = require('../controllers/admin.controller');
 
 router.get('/pro', productoCotroller.obtenerProductos); 
-router.get('/pro/:id', productoCotroller.obtenerProductos); 
+router.get('/pro/:id', productoCotroller.obtenerProductosId); 
 router.post('/pro/add', productoCotroller.crearProductos);
 router.put('/pro/edi', productoCotroller.editarProductos);
 router.delete('/pro/:id', productoCotroller.eliminarProductos);
 router.get('/usu', productoCotroller.obtenerUsuario);
-router.get('/usu/:id', productoCotroller.obtenerUsuario);
+router.get('/usu/:id', productoCotroller.obtenerProductosId);
 router.get('/usu/edi', productoCotroller.editarUsuario);
 router.get('/usu/:id', productoCotroller.eliminarUsuario);
 router.get('/ven', productoCotroller.obtenerVentas);
