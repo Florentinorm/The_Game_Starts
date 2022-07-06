f305eb72a865b1c2b8705c41db96e07bd351e573
an222xd <joseantonio222xd@gmail.com>
2022-07-05 20:29:40 -0500

    Detalle Venta

--- Backend/controllers/admin.controller.js
+++ Backend/controllers/admin.controller.js
@@ -1,5 +1,11 @@
 const pool = require('../config/db');
 
+/**
+ * Metodo obtener productos
+ * 
+ * @param {result} res resultado de la consulta
+ * @param {err} next Error
+ */
 exports.obtenerProductos = async (req, res, next) => {
     try {
         const result = await pool.execute(`SELECT id_producto, nomProducto, desProducto, cantidad,
@@ -14,6 +20,13 @@ exports.obtenerProductos = async (req, res, next) => {
     }
 }
 
+/**
+ * Metodo obtener producto por id
+ * 
+ * @param {params.id} req atrapar el id del producto que fue seleccionado
+ * @param {result} res Regresa el resultado de la consulta
+ * @param {err} next Error
+ */
 exports.obtenerProductosId = async (req, res, next) => {
     id_producto = req.params.id;
     try {
@@ -28,6 +41,14 @@ exports.obtenerProductosId = async (req, res, next) => {
     }
 }
 
+/**
+ * 
+ * Metodo para agregar producto
+ * 
+ * @param {body} req Traer todos los datos del producto para agregar un nuevo producto
+ * @param {status(200)} res Se agrego exitosamente
+ * @param {err} next Error
+ */
 exports.crearProductos = async (req, res, next) => {
     console.log(req.body);
     nomProducto = req.body.nomProducto;
@@ -52,6 +73,14 @@ exports.crearProductos = async (req, res, next) => {
     }
 }
 
+/**
+ * 
+ * Metodo para editar producto
+ * 
+ * @param {body} req Traer datos a editar
+ * @param {status(200)} res Respuesta de exito
+ * @param {*} next Error
+ */
 exports.editarProductos = async (req, res, next) => {
     console.log(req.body);
     id_producto = req.body.id_producto;
@@ -78,6 +107,13 @@ exports.editarProductos = async (req, res, next) => {
     }
 }
 
+/**
+ * Metodo eliminar producto por id
+ * 
+ * @param {params.id} req Traer id
+ * @param {status(200)} res Eliminado correctamente
+ * @param {err} next Error 
+ */
 exports.eliminarProductos = async (req, res, next) => {
     id_producto = req.params.id;
     try {
@@ -91,6 +127,12 @@ exports.eliminarProductos = async (req, res, next) => {
     }
 }
 
+/**
+ * Metodo optener Usuarios
+ * 
+ * @param {result} res Enviar todos los usuarios de la base de datos
+ * @param {err} next Error
+ */
 exports.obtenerUsuario = async (req, res, next) => {
     try {
         const result = await pool.execute(`SELECT id_usuario, nombre, apellido_paterno,
@@ -105,6 +147,13 @@ exports.obtenerUsuario = async (req, res, next) => {
     }
 }
 
+/**
+ * Metodo obtener usuario por id
+ * 
+ * @param {params.id} req Traer id del usuario
+ * @param {result} res Datos del usuario seleccionado
+ * @param {err} next Error
+ */
 exports.obtenerUsuario = async (req, res, next) => {
     id_usuario = req.params.id;
     try {
@@ -120,6 +169,14 @@ exports.obtenerUsuario = async (req, res, next) => {
     }
 }
 
+/**
+ * 
+ * Metodo editar usuario
+ * 
+ * @param {body} req Traer los datos a editar
+ * @param {status(200)} res Editado correctamente
+ * @param {err} next Error 
+ */
 exports.editarUsuario = async (req, res, next) => {
     console.log(req.body);
     id_usuario = req.body.id_usuario;
@@ -146,6 +203,14 @@ exports.editarUsuario = async (req, res, next) => {
     }
 }
 
+/**
+ * 
+ * Metodo eliminar usuario
+ * 
+ * @param {params.id} req Traer id del usuario a eliminar
+ * @param {status(200)} res Eliminado correctamente
+ * @param {err} next Error
+ */
 exports.eliminarUsuario = async (req, res, next) => {
     id_usuario = req.params.id;
     try {
@@ -160,10 +225,43 @@ exports.eliminarUsuario = async (req, res, next) => {
     }
 }
 
+/**
+ * 
+ * Metodo obter Ventas
+ * 
+ * @param {result} res Enviar ventas
+ * @param {err} next Error
+ */
 exports.obtenerVentas = async (req, res, next) => {
     try {
         const result = await pool.execute(`SELECT id_venta, nomVenta, desVenta, canVenta, id_producto,
          id_usuario, totalVenta FROM ventas`);
+        res.status(200).json(result);
+
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+}
+
+/**
+ * 
+ * Metodo obtener detalle venta
+ * 
+ * @param {params.id} req Traer id de la venta seleccionada
+ * @param {result} res Enviar venta seleccionada
+ * @param {err} next Error
+ */
+exports.obtenerDetalleVenta = async (req, res, next) => {
+    id_venta = req.params.id;
+    try {
+        const result = await pool.execute(`SELECT ven.nomVenta, ven.desVenta, ven.canVenta,
+         ven.id_producto, pro.nomProducto, ven.id_usuario, 
+         CONCAT(usu.nombre," ",usu.apellido_paterno," ",usu.apellido_materno) nombre, 
+         ven.totalVenta FROM ventas ven INNER JOIN productos pro ON pro.id_producto = ven.id_producto 
+         INNER JOIN usuario usu ON usu.id_usuario = ven.id_usuario WHERE ven.id_venta = ?`, [id_venta]);
         res.status(200).json(result);
 
     } catch (err) {
--- Backend/routes/admin.route.js
+++ Backend/routes/admin.route.js
@@ -5,12 +5,13 @@ const productoCotroller = require('../controllers/admin.controller');
 router.get('/pro', productoCotroller.obtenerProductos); 
 router.get('/pro/:id', productoCotroller.obtenerProductosId); 
 router.post('/pro/add', productoCotroller.crearProductos);
-router.put('/pro/edi', productoCotroller.editarProductos);
+router.post('/pro/edi', productoCotroller.editarProductos);
 router.delete('/pro/:id', productoCotroller.eliminarProductos);
 router.get('/usu', productoCotroller.obtenerUsuario);
 router.get('/usu/:id', productoCotroller.obtenerProductosId);
-router.get('/usu/edi', productoCotroller.editarUsuario);
-router.get('/usu/:id', productoCotroller.eliminarUsuario);
+router.post('/usu/edi', productoCotroller.editarUsuario);
+router.delete('/usu/:id', productoCotroller.eliminarUsuario);
 router.get('/ven', productoCotroller.obtenerVentas);
+router.get('/ven/:id', productoCotroller.obtenerDetalleVenta);
 
 module.exports = router;
